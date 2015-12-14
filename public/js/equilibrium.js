ccc048974f3800b95cc68a3f9be964ac034a3e7b
Nartub <maimar@gmail.com>
2015-12-14 14:35:51 -0300

    JS assets

---
+++ public/js/equilibrium.js
@@ -0,0 +1,19 @@
+$(function(){
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+
+        var form = $(e.target);
+
+        form.ajaxSubmit({
+            success: function(data) {
+                if (data.status == 'ok') {
+                    window.location = data.url;
+                } else {
+                    alert('pifiaste');
+                }
+            }
+        });
+    });
+
+});
